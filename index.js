0d5368c38e051a7e1f0635411559db0e043ce211
Sampson Orson Jackson <sampson.orson@gmail.com>
2022-02-19 13:33:16 +0100

    feat: display basic information about Sampson Orson Jackson

---
+++ index.js
@@ -0,0 +1,28 @@
+import boxen from 'boxen';
+import chalk from 'chalk';
+
+const w = chalk.white;
+const c = chalk.cyanBright;
+const b = chalk.bold;
+const m = chalk.hex('#FF5A89');
+
+const cardOptions = {
+    padding: 1,
+    margin: 1,
+    borderStyle: "round",
+    borderColor: "#ffffff"
+};
+
+const content = `
+${b("Sampson Orson Jackson")}
+${w("Full-stack .NET Developer + Angular")}
+
+${w("Website")}            ${m("https://sampsonorson.com")}
+${w("GitHub")}             ${m("https://github.com/dev-sampsonorson")}
+${w("Twitter")}            ${m("https://twitter.com/sampsonorson")}
+${w("LinkedIn")}           ${m("https://linkedin.com/in/sampsonorson")}
+`;
+
+const card = boxen(content, cardOptions);
+
+console.log(card);
